7827f1025851e1465b269086c389a7fb3b264894
CryptoN999 <nicholasgallucci4@gmail.com>
2020-04-20 14:46:40 -0700

    basic js refresher

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,20 @@
+/***************** 
+var firstName = 'Nick'; // double quotes "" also works for a string
+console.log(firstName);
+
+var lastName = 'Gallucci';
+var age = 26;
+console.log(firstName, lastName, age);
+
+var job;
+job = 'Team-Lead at Lambda School';
+console.log(job)
+
+var timeEmployed = '8 months';
+console.log(timeEmployed);   // going through everything from basic to advanced concepts to increase my skills
+*/
+
+/*****************
+ *VARIABLE MUTATION AND TYPE COERCION:
+ */
+
